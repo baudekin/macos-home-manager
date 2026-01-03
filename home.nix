be30939d292af6d8985cbb6553ba13611c78d7f3
Michael A. Bodkin <bodkin@acm.org>
2026-01-03 13:27:49 -0500

    Added Brave Browser.

--- home.nix
+++ home.nix
@@ -19,6 +19,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    pkgs.vscode
+    
     #pkgs.emacs
     # Required by Emacs 
     pkgs.cmake # vterm requires cmake to build its C module
