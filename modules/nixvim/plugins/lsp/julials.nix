3e4c821d50b25020754e9c9d66cfd4c5ed53bf96
Michael A. Bodkin <bodkin@acm.org>
2026-01-29 13:27:32 -0500

    Basic JETLS setup.

--- modules/nixvim/plugins/lsp/julials.nix
+++ modules/nixvim/plugins/lsp/julials.nix
@@ -1,7 +1,18 @@
 { lib, pkgs, ... }:
 {
-  julia-vim.enable = true;
-  lsp.servers.julials = {
+  programs.nixvime.extraPlugins = with pkgs.vimPlugins; [
+    jetls
+  ];
+  lsp.servers.jetls = {
     enable = true;
+    config = {
+      filetypes = [ "julia" ];
+      root_markers = [ "Project.toml" ];
+      cmd = [
+        "jetls"
+        "--threads=auto"
+        "--"
+      ];
+    };
   };
 }
